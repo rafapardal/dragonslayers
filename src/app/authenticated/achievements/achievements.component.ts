b2b4acebab1cae44e84eab7476dd8dd6e786f18c
Rafa Pardal <rafapardal07@hotmail.com>
2017-01-12 06:34:12 +0000

    achievements improves

--- src/app/authenticated/achievements/achievements.component.ts
+++ src/app/authenticated/achievements/achievements.component.ts
@@ -9,21 +9,31 @@ import { AuthService } from '../../services/auth.service';
 })
 export class AchievementsComponent implements OnInit {
   private achievementList = [];
+  private ids = [];
 
 
-  constructor(private achievements: AchievementsService, private auth: AuthService) { }
+  constructor(private achievements: AchievementsService, private auth: AuthService) {}
 
   ngOnInit() {
-    let ids = [];
-    this.auth.getUserAchievements(this.auth.getUser()).subscribe(result => {
-      for (let entry of result.achievements) {
-        ids.push(entry);
+    this.getIds();
+    this.getAchievementList()
+  }
+
+  getIds() {
+      this.ids = JSON.parse(sessionStorage.getItem("ids"));
+      if (this.ids === null) {
+        this.auth.getUserAchievements(this.auth.getUser()).subscribe(result => {
+          sessionStorage.setItem("ids", JSON.stringify(result.achievements));
+          this.ids = JSON.parse(sessionStorage.getItem("ids"));
+        });
       }
-    });
+  }
+
+  getAchievementList() {
     this.achievements.getList().subscribe(result => {
       for (let entry of result) {
         let done = false;
-        if (ids.indexOf(entry._id) > -1) done = true;
+        if (this.ids.indexOf(entry._id) > -1) done = true;
         this.achievementList.push({'result': entry, 'done': done});
       }
     });
--- src/app/services/achievements.service.ts
+++ src/app/services/achievements.service.ts
@@ -7,10 +7,23 @@ export class AchievementsService {
   constructor(private http: Http) { }
 
   public getList() {
+    /*
+    let list = JSON.parse(sessionStorage.getItem("achievementsList"));
+    if (!list) {
       return this.http.get('/api/achievement/')
        .map((response: Response) => {
+         sessionStorage.setItem("achievementsList", JSON.stringify(response));
          return response.json();
        });
+    } else {
+      return list.json();
     }
+    */
+    return this.http.get('/api/achievement/')
+     .map((response: Response) => {
+       return response.json();
+     });
+
+  }
 
 }
